a45b346c7b62ded7c3021bd0e13ee565b9a81354
rishav-jha-mech <rishav.rj74@gmail.com>
2022-10-22 19:04:43 +0530

    Navbar and Home section made initially

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function menuBtnFunction(menuBtn) {
+    menuBtn.classList.toggle("active");
+    var menu = document.getElementById("nav-list");
+    // setTimeout(() => {
+        menu.classList.toggle("tog-h");
+    // }, 650);
+}
