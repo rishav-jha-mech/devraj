ad3dda80eebe505240b2beccb40311eebc24f56a
rishav-jha-mech <rishav.rj74@gmail.com>
2022-10-23 10:11:05 +0530

    About me section made fixed the nav bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
+const menu = document.getElementById("nav-list");
+const main = document.getElementById("main");
+
 function menuBtnFunction(menuBtn) {
     menuBtn.classList.toggle("active");
-    var menu = document.getElementById("nav-list");
-    // setTimeout(() => {
-        menu.classList.toggle("tog-h");
-    // }, 650);
+    menu.classList.toggle("tog-h");
+    main.classList.toggle("d-none");
 }
