8d6ef209d11a9b1980dc8d9bbaa35d728ac35e0e
rishav-jha-mech <rishav.rj74@gmail.com>
2022-10-24 05:43:52 +0530

    Qualifications section made

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
 const menu = document.getElementById("nav-list");
 const main = document.getElementById("main");
+const eduBtn = document.getElementById("educ-btn");
+const workBtn = document.getElementById("work-btn");
+const eduContainer = document.getElementById("educ-container");
+const workContainer = document.getElementById("work-container");
 
 function menuBtnFunction(menuBtn) {
     menuBtn.classList.toggle("active");
     menu.classList.toggle("tog-h");
     main.classList.toggle("d-none");
+}
+
+function toggleQual() {
+    eduBtn.classList.toggle("active");
+    workBtn.classList.toggle("active");
+    eduContainer.classList.toggle("d-none");
+    workContainer.classList.toggle("d-none");
 }
